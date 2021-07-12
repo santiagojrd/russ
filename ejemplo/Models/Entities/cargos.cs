58d928510ccdf3ac1a83c95dad29ff3863a1d51b
Santiago Jaramillo Durango <sjaramillo409@misena.edu.co>
2021-07-12 12:36:20 -0500

    cree una nueva carpeta y dentro de esta una clase llamada cargos

---
+++ ejemplo/Models/Entities/cargos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ejemplo.Models.Entities
+{
+    public class cargos
+    {
+    }
+}
